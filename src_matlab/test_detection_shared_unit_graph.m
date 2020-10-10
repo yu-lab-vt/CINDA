5a40fd472d1c4fe6d6758e2af913293c57e54e78
ccwang92 <ccwang1992@gmail.com>
2020-10-10 12:10:07 -0400

    sim

--- src_matlab/test_detection_shared_unit_graph.m
+++ src_matlab/test_detection_shared_unit_graph.m
@@ -1,6 +1,6 @@
 addpath('src_matlab/', 'src_c/');
 
-load('graph_nonMOT.mat','arcs');
+load('graph_detection_shared.mat','arcs');
 
 n_nodes = max(max(arcs(:,1)), max(arcs(:,2)));
 
@@ -30,6 +30,32 @@ tic;
 toc;
 
 
+%% min-cost flow
+n_nodes = max(max(arcs(:,1)), max(arcs(:,2)));
+
+dat_in = arcs;
+dat_in(isinf(dat_in(:,3)),:) = []; % remove inf arcs
+dat_in(:,3) = floor(dat_in(:,3)*1e7); % change to integer cost
+
+num_node = n_nodes;
+num_arc = size(dat_in,1);
+
+
+excess_node = [1 n_nodes];
+excess_flow = [100 -100];
+
+low = zeros(1,num_arc);
+acap = ones(1,num_arc);
+
+tail = dat_in(:,1)';
+head = dat_in(:,2)';
+cost = dat_in(:,3)';
+
+scale = 12;
+tic;
+[cost_all,~,~,~, track_vec] = cinda_mex(scale, num_node, num_arc, ...
+    excess_node, excess_flow, tail, head, low, acap, cost);
+toc;
 
 
 
